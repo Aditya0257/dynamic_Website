4b8440974363075f1a104b701873efdee013374f
Aditya0257 <adityasinghthakur.0257@gmail.com>
2023-04-11 10:48:50 +0530

    commit

--- src/models/usermessage.js
+++ src/models/usermessage.js
@@ -2,35 +2,32 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3,
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email id");
+      }
     },
-    email: {
-        type: String,
-        required: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Invalid email id")
-            }
-        }
-    },
-    phone: {
-        type: Number,
-        required: true,
-        min: 10,
-        // max: 10,
-    },
-    message: {
-        type: String,
-        required: true,
-        minLength: 3,
-    },
-    
-})
+  },
+  phone: {
+    type: Number,
+    required: true,
+    min: 10,
+  },
+  message: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+});
 
-//* we need a collection 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
